410b844f775fe604d89890610fdbb95ec898dbee
AZKvgkv <AZK19991219@outlook.com>
2026-02-20 17:31:42 +0800

    feat(parser): 添加对整数字面量的解析功能
    - 在 `import` 部分添加了 `strconv` 包，用于整数字符串的解析。
    - 在 `New` 函数中注册了 `token.INT` 的前缀解析函数 `p.parseIntegerLiteral`，以便解析整数字面量。
    - 新增了 `parseIntegerLiteral` 函数，该函数尝试将当前标记的字面量解析为整数，并处理解析错误，将解析结果存储在 `ast.IntegerLiteral` 结构体中。

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
+	"strconv"
 )
 
 const (
@@ -52,6 +53,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.nextToken()
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	return p
 }
@@ -163,4 +165,16 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 }
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
